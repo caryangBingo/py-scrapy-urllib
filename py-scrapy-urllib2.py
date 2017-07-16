219bde43ebf7b7c0ee7f8cf7b55961b3e2a8242e
caryangBingo <zhiyuan1005873@gmail.com>
2017-07-16 22:48:04 +0800

    init commit

---
+++ py-scrapy-urllib2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-07-16 22:47:02
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
