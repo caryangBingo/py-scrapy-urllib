2de50ae8191e9d390270c283ae57c009e3921189
caryangBingo <zhiyuan1005873@gmail.com>
2017-07-17 00:52:00 +0800

    add urllib

--- py-scrapy-urllib2.py
+++ py-scrapy-urllib2.py
@@ -6,3 +6,23 @@
 # @Version : $Id$
 
 import os
+import sys
+import ssl
+import urllib.request
+from importlib import reload
+
+#第一个爬虫练习
+"""
+url= "http://www.baidu.com"
+data = urllib.request.urlopen(url).read()#
+#data = data.decode('UTF-8')
+print (data)
+"""
+
+url = "http://www.douban.com/"
+request = urllib.request.Request(url)
+ssl._create_default_https_context = ssl._create_unverified_context
+responseurl = urllib.request.urlopen(request)
+data = responseurl.read()
+#data = data.decode('utf-8')
+print(data)
